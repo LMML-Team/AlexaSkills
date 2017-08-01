1e6ecdbfeb580a0223fff3402c26d8c0ca5bddfc
Anthony Cavallaro <ajcavallaro10@gmail.com>
2017-08-01 13:55:07 -0400

    Added function

--- Stock Info/stockinfo.py
+++ Stock Info/stockinfo.py
@@ -3,6 +3,7 @@ from flask_ask import Ask, statement, question, session
 import requests
 import unidecode
 import json
+import numpy as np
 
 app = Flask(__name__)
 ask = Ask(app, '/')
@@ -26,5 +27,28 @@ def get_stock():
     #Implementing once AJ finishes code
     return statement("You've reached the stock intent zone.")
 
+def output(ticker):
+    sess = requests.Session()
+    url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={}&outputsize=compact&apikey=GXTRIS3LY4DDIF3S'.format(
+        ticker)
+    html = sess.get(url)
+
+    time_series_day = json.loads(html.content.decode('utf-8'))
+    days = time_series_day['Time Series (Daily)']
+    ordered = OrderedDict(sorted(days.items(), key=lambda t: t[0], reverse=True))
+
+    count = 0
+    values = []
+    for key in ordered:
+        values.append(ordered[key]['4. close'])
+        count += 1
+        if count == 2:
+            break
+
+    current_price = float(values[0])
+    prev_close = float(values[1])
+    percent_increase = np.round(abs(prev_close - current_price) / prev_close * 100, 2)
+
+    return "The price of {} is currently {}, and is up {} percent today/".format(ticker, current_price, percent_increase)
 
 
