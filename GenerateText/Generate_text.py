748e2ea1d1c24c9b7d57df409b83145ed59fd1b0
Anthony Cavallaro <ajcavallaro10@gmail.com>
2017-08-03 13:47:09 -0400

    Added docs and comments

--- GenerateText/Generate_text.py
+++ GenerateText/Generate_text.py
@@ -7,12 +7,49 @@ import matplotlib.pyplot as plt
 
 def unzip(pairs):
     """
+    Splits list of pairs (tuples) into separate lists
+    
+    Parameter(s)
+    --------------
+    pairs(list of tuples):
+        List of pairs to be split
+    
+    Return(s)
+    --------------
+    list 1:
+        List containing the first term of all pairs
+        
+    list 2:
+        List containing the second term of all pairs
+        
+    Example(s)
+    --------------
+    
+    pairs = [("a", 1), ("b", 2)] --> ["a", "b"] and [1, 2]
+        
     """
-    return tuple(zip(*pairs))
+    return [zip(*pairs)]
 
 
 def normalize(counter):
     """ 
+    Convert counter to a list of (letter, frequency) pairs, sorted in descending order of frequency
+    
+    Parameter(s)
+    --------------
+    counter (Counter-instance):
+        Counter to be converted. Letters and their # of occurrences
+        
+    Return(s)
+    --------------
+    list[tuples]:
+        List containing tuples of (letter, frequency) pairs
+        
+    Example(s)
+    --------------
+        counter = Counter({'a': 1, 'b': 3})
+        normalize(counter) = [('b', 0.75), ('a', 0.25)]
+    
     """
     total = sum(counter.values())
     return [(char, cnt / total) for char, cnt in counter.most_common()]
@@ -20,29 +57,45 @@ def normalize(counter):
 
 def train_lm(text, n):
     """ 
-        Parameters
-        -----------
-        text: str 
-            A string (doesn't need to be lowercased).
-        n: int
-            The length of n-gram to analyze.
+    Train character-based n-gram language model.
+    Given a sequence of n-1 characters, what is the probability distribution
+    for the n-th character in the sequence.
+    
+    Parameter(s)
+    --------------
+    text(str):
+        A string (any case)
+        
+    n(int):
+        Order of n-gram model
 
-        Returns
-        -------
-        A dict that maps histories (strings of length (n-1)) to lists of (char, prob) 
-        pairs, where prob is the probability (i.e frequency) of char appearing after 
-        that specific history. For example, if
+    Returns
+    --------------
+    dict({ strings of len n-1: list[(char, probability)]:
+        Maps histories of characters (led by ~ if # of proceeding chars < n-1)
+        to list of pairs containing chars and the probability of them appearing
+        after that particular history
+    
+    Example(s)
+    --------------
+    text = "cacao"
+    n = 3
+    train_lm(text, n) ={'ac': [('a', 1.0)],
+                        'ca': [('c', 0.5), ('o', 0.5)],
+                        '~c': [('a', 1.0)],
+                        '~~': [('c', 1.0)]}
 
     """
+    # Initialize lm and history
     raw_lm = defaultdict(Counter)
     history = "~" * (n - 1)
 
-    # count number of times characters appear following different histories
+    # Count number of times characters appear following different histories
     for x in text:
         raw_lm[history][x] += 1
         history = history[1:] + x
 
-    # create final dictionary by normalizing
+    # Create final dictionary by normalizing
     lm = {history: normalize(counter) for history, counter in raw_lm.items()}
 
     return lm
@@ -50,47 +103,64 @@ def train_lm(text, n):
 
 def generate_letter(lm, history):
     """ 
-        Parameters
-        ----------
-        lm: Dict[str, Tuple[str, float]] 
-            The n-gram language model. I.e. the dictionary: history -> (char, freq)
+    Generates a letter according to the probability distribution of the specified history
+    
+    Parameter(s)
+    --------------
+    lm (Dict{str: Tuple(str, float)}):
+        The n-gram language model relating history to letter probabilities
 
-        history: str
-            A string of length (n-1) to use as context/history for generating 
-            the next character.
+    history (str):
+        Length n-1 context for use as key in language model
 
-        Returns
-        -------
-        str
-            The predicted character. '~' if history is not in language model.
+    Returns
+    --------------
+    str:
+        The predicted character. '~' if history is not in language model.
     """
+
+    # Default behaviour if history is not in the model
     if not history in lm:
         return "~"
+
+    # Uses unzip function to split the letters from their probabilities
     letters, probs = unzip(lm[history])
+
+    # Randomly selects and returns a letter from letters with its p in probs
     i = np.random.choice(letters, p=probs)
     return i
 
 
 def generate_text(lm, n, nletters=100):
     """ 
-        Parameters
-        ----------
-        lm: Dict[str, Tuple[str, float]] 
-            The n-gram language model. I.e. the dictionary: history -> (char, freq)
-        n: int
-            Order of n-gram model.
-        nletters: int
-            Number of letters to randomly generate.
+    Generates nletters random letters according to n-gram language model, lm
+    
+    Parameters
+    --------------
+    lm (Dict{str: Tuple(str, float)}):
+        The n-gram language model relating history to letter probabilities
+        
+    n (int):
+        Order of n-gram model.
+        
+    nletters: int
+        Number of letters to randomly generate.
 
-        Returns
-        -------
-        str
-            Model-generated text.
+    Returns
+     --------------
+    str
+        Model-generated text.
     """
+
+    # Initializes history and text
     history = "~" * (n - 1)
     text = []
+
+    # uses generate_letter function to generate text that is nletters long
     for i in range(nletters):
         c = generate_letter(lm, history)
         text.append(c)
         history = history[1:] + c
+
+    # Joins and returns the characters in text
     return "".join(text)
